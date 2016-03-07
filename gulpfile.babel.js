a3dc4421119a638ff8578d50f00c6eefa7447ea8
io-monad <iride.monad@gmail.com>
2016-03-07 13:47:03 +0900

    Stop reading schema and now build a concatenated schema file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,31 @@
 import gulp from "gulp";
 import gulpLoadPlugins from "gulp-load-plugins";
 import {exec} from "child_process";
+import path from "path";
 
 const $ = gulpLoadPlugins()
 
 gulp.task("default", ["build"]);
 gulp.task("json", $.sequence("lint", "validate", "format"));
+gulp.task("build", ["build:src", "build:schemas"]);
 
-gulp.task("build", () => {
+gulp.task("build:src", ["build:schemas"], () => {
   return gulp.src("src/**/*.js")
     .pipe($.babel())
     .pipe(gulp.dest("lib"))
+});
+
+gulp.task("build:schemas", () => {
+  return gulp.src("schemas/*.json", { base: path.join(__dirname, "schemas") })
+    .pipe($.wrap('  "<%= file.relative.replace(/\\.json$/, "") %>": <%= JSON.stringify(contents) %>'))
+    .pipe($.concat("schemas.js", { newLine: ",\n" }))
+    .pipe($.wrap([
+      "// This is auto-generated file",
+      "export default {",
+      "<%= contents %>",
+      "}"
+    ].join("\n")))
+    .pipe(gulp.dest("src"))
 });
 
 gulp.task("test", () => {
--- src/textlint-registry.js
+++ src/textlint-registry.js
@@ -1,10 +1,8 @@
 'use strict';
 
-import fs from 'fs';
-import path from 'path';
+import schemas from "./schemas";
 
 const TEXTLINT_RULE_PREFIX = 'textlint-rule-';
-const SCHEMA_DIR = path.join(__dirname, '..', 'schemas');
 
 function stripRulePrefix(ruleName) {
   if (ruleName.slice(0, TEXTLINT_RULE_PREFIX.length) === TEXTLINT_RULE_PREFIX) {
@@ -15,26 +13,18 @@ function stripRulePrefix(ruleName) {
 }
 
 function getAvailableSchemas() {
-  return new Promise((resolve, reject) => {
-    fs.readdir(SCHEMA_DIR, (err, files) => {
-      if (err) return reject(err);
-      resolve(files.map((file) => stripRulePrefix(file).replace(/\.json$/, "")));
-    });
-  });
+  return Promise.resolve(
+    Object.keys(schemas).map((key) => stripRulePrefix(key))
+  );
 }
 
 function getSchema(ruleName) {
   ruleName = TEXTLINT_RULE_PREFIX + stripRulePrefix(ruleName);
-  return new Promise((resolve, reject) => {
-    fs.readFile(path.join(SCHEMA_DIR, `${ruleName}.json`), "utf-8", (err, content) => {
-      if (err) return reject(err);
-      try {
-        resolve(JSON.parse(content));
-      } catch (e) {
-        reject(e);
-      }
-    });
-  });
+  if (schemas[ruleName]) {
+    return Promise.resolve(schemas[ruleName]);
+  } else {
+    return Promise.reject(new Error(`Unknown schema name: ${ruleName}`));
+  }
 }
 
 export default {
